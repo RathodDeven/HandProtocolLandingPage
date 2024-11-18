101e33028762c476f48cfcb071048c3d269bd811
cryptokoh <cryptokoh@gmail.com>
2024-11-18 09:46:16 -0600

    Update config.ts
    # Project Order Update
    
    We've set the final order of the featured projects:
    1. Documentary About Nigerian Human Trafficking (Existential Zoomer)
    2. Digital Street Music (Jose Cabrera)
    3. BloomersTV (Deven Rathod)
    
    Also fixed a small typo in the Digital Street Music description. All other project details remain unchanged.

--- utils/config.ts
+++ utils/config.ts
@@ -23,19 +23,6 @@ export const SPLINE_SCENE_IFRAME_LINK =
 export const INIT_PROJECTS = [
   {
     id: 1,
-    name: 'Open Source Live Streaming Platform on Lens Protocol',
-    by: 'Deven Rathod',
-    link: 'https://explorer.gitcoin.co/#/projects/0x16429f74cbb2cef6f9f48481b6c8bc49fa12989d75c1f9d82462f8bb91f079eb',
-    images: [
-      'https://ik.imagekit.io/lens/media-snapshot/3e3c106e222a05c69767af09bee4dade1c658e6a2b4c853e7eeab59f2467045b.png',
-      'https://d16c97c2np8a2o.cloudfront.net/ipfs/bafkreidepxgjovvutgi3nhwvp644wu6onpnzgucu6pdpppxh7bo5kv3tn4',
-      'https://ik.imagekit.io/lens/media-snapshot/tr:w-1000/8391806428454e492506a88863c9d3b624e43345861006cf893f24bcbfabde0d.png'
-    ],
-    shortDescription:
-      'BloomersTV aims to transform live streaming by giving creators full control over their earnings and distributing streams across multiple platforms for greater reach. It fosters a creator-first ecosystem that encourages open participation and community-driven growth.'
-  },
-  {
-    id: 2,
     name: 'Documentary About Nigerian Human Trafficking',
     by: 'Existential Zoomer',
     images: [
@@ -47,22 +34,8 @@ export const INIT_PROJECTS = [
       'Traditional organizations declined to help us. Web3 is making a full-length documentary possible. Our documentary plans to educate communities about predatory practices used by traffickers and establish a blueprint for Nigerians to leverage Web3 to address local issues. ',
     link: 'https://giveth.io/project/documentary-about-nigerian-human-trafficking'
   },
-  // {
-  //   id: 3,
-  //   name: 'Huottoja University Of Ancestral Knowledge',
-  //   images: [
-  //     'https://giveth.mypinata.cloud/ipfs/QmRVghjwJmsH3WZNQxEPhz7ta8y8u41jeavBswrDeyrWEp',
-  //     'https://giveth.mypinata.cloud/ipfs/QmVwdqqD2z378QBv9wXM9JeHtK4MXTbBLJTNmMdR6ahe3U',
-  //     'https://giveth.mypinata.cloud/ipfs/Qme7N8XhDenKKAtymSyoh839qPfyWCRahXxsTvk7ym9S7P',
-  //     'https://giveth.mypinata.cloud/ipfs/QmT2tGgQMRF4VzycWXgDvKzsBcEeoKyPqzqtopYh1NLNSy'
-  //   ],
-  //   by: 'Jose Cabrera',
-  //   link: 'https://giveth.io/project/huottoja-university-of-ancestral-knowledge',
-  //   shortDescription:
-  //     'In the Venezuelan Amazon, on the edges of the Orinoco River, lives the Huöttöja or Piaroa community, who are known for their peace and harmony with nature. Their Kuami Kiachi (strength in the heart) philosophy promotes calm and temperance in all circumstances, a reflection of their ancestral lifestyle.'
-  // }
   {
-    id: 3,
+    id: 2,
     name: 'DIGITAL Street Musician Doing Free Live Concerts',
     images: [
       'https://giveth.mypinata.cloud/ipfs/QmThFYyWhirw15savs6DeQxTdNoz3o5fsNzHE4aRLkFTre',
@@ -73,7 +46,19 @@ export const INIT_PROJECTS = [
     by: 'Jose Cabrera',
     link: 'https://giveth.io/project/digital-street-musician-doing-free-live-concerts',
     shortDescription:
-      "The core of these iniative revolves around the concept of 'Digital Street Music'. I've been offering my music as a public good for various Decentralized Autonomous Organizations (DAOs) and the organic web3 sphere."
+      "The core of this initiative revolves around the concept of 'Digital Street Music'. I've been offering my music as a public good for various Decentralized Autonomous Organizations (DAOs) and the organic web3 sphere."
+  },
+  {
+    id: 3,
+    name: 'Open Source Live Streaming Platform on Lens Protocol',
+    by: 'Deven Rathod',
+    link: 'https://explorer.gitcoin.co/#/projects/0x16429f74cbb2cef6f9f48481b6c8bc49fa12989d75c1f9d82462f8bb91f079eb',
+    images: [
+      'https://ik.imagekit.io/lens/media-snapshot/3e3c106e222a05c69767af09bee4dade1c658e6a2b4c853e7eeab59f2467045b.png',
+      'https://d16c97c2np8a2o.cloudfront.net/ipfs/bafkreidepxgjovvutgi3nhwvp644wu6onpnzgucu6pdpppxh7bo5kv3tn4',
+      'https://ik.imagekit.io/lens/media-snapshot/tr:w-1000/8391806428454e492506a88863c9d3b624e43345861006cf893f24bcbfabde0d.png'
+    ],
+    shortDescription:
+      'BloomersTV aims to transform live streaming by giving creators full control over their earnings and distributing streams across multiple platforms for greater reach. It fosters a creator-first ecosystem that encourages open participation and community-driven growth.'
   }
-  
-]
+];
